48f2984672dfaf0cadf60a5a14ac9ed5ce320435
Comrade17th <ravshannedonat@gmail.com>
2022-09-14 09:37:53 +0300

    added lb2

---
+++ ASD/lb2_list/asd_lb2_v6/asd_lb2_v6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd_lb2_v6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
