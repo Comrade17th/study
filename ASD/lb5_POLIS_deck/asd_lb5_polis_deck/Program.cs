dff36974c60c2d0a37d084e09da6691a6c8c742c
ravshan <ravshannedonat@gmail.com>
2022-09-15 19:30:12 +0300

    lb5_true7_q1_polis done

---
+++ ASD/lb5_POLIS_deck/asd_lb5_polis_deck/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd_lb5_polis_deck
+{
+    internal class Program
+    {
+
+        static public int calcPolis(string sequence) // в отдельную функцию можно вынести проверку строки на правильнось, но это не надо никому
+        {
+            int res = 0;
+            string[] parts = sequence.Split();
+            Stack<int> ints = new Stack<int>();
+            for(int i = 0; i < parts.Length; i++)
+            {
+                int tmp;
+                switch(parts[i])
+                {
+                    case "+":
+                        tmp = ints.Pop();
+                        ints.Push(ints.Pop() + tmp);
+                        break;
+                    case "-":
+                        tmp = ints.Pop();
+                        ints.Push(ints.Pop() - tmp);
+                        break;
+                    case "/":
+                        tmp = ints.Pop();
+                        ints.Push(ints.Pop() / tmp);
+                        break;
+                    case "*":
+                        tmp = ints.Pop();
+                        ints.Push(ints.Pop() * tmp);
+                        break;
+                    default:
+                        ints.Push(Convert.ToInt32(parts[i]));
+                        break;
+                }
+            }
+            res = ints.Pop();
+            return res;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] test = {
+                "8 9 + 1 7 - *", // - 102
+                "3 4 +", // 7
+                "7 7 /" // 1
+            };
+            for (int i = 0; i < test.Length; i++)
+                Console.WriteLine($"{test[i]} = {calcPolis(test[i])}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ASD/lb5_POLIS_deck/asd_lb5_polis_deck/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("asd_lb5_polis_deck")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("asd_lb5_polis_deck")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("142e9db6-79ed-45c8-96a0-990c8cc05e4d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
