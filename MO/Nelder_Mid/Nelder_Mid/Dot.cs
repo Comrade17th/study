52e43e585c37e9dafca4f7261b53cd4c2b6755e9
ravshan <ravshannedonat@gmail.com>
2022-11-25 20:49:32 +0300

    NelderMid

---
+++ MO/Nelder_Mid/Nelder_Mid/Dot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Nelder_Mid
+{
+    internal class Dot: IComparable<Dot>
+    {
+        double x;
+        double y;
+        const double N = 13.0;
+
+        public Dot()
+        {
+            
+        }
+
+        public Dot(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public double fun
+        {
+            get { return N * Pow(x, 2) + N * Pow(y, 2) - N * x * y + y; ; }
+        }
+
+        public int CompareTo(Dot dot)
+        {
+            if (dot is null) throw new ArgumentException("Некорректное значение параметра");
+            return (int)(fun - dot.fun);
+        }
+    }
+}
---
+++ MO/Nelder_Mid/Nelder_Mid/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelder_Mid
+{
+    internal class Triangle
+    {
+        Dot[] dots = new Dot[3];
+        public Triangle()
+        {
+
+        }
+        public Triangle(Dot dt0, Dot dt1, Dot dt2)
+        {
+            dots[0] = dt0;
+            dots[1] = dt1;
+            dots[2] = dt2;
+        }
+    }
+}
