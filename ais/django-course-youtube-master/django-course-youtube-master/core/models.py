2bd08a2f7b1f13a14b4fdfe2addea3b43515b688
ravshan <ravshannedonat@gmail.com>
2022-12-18 17:54:18 +0300

    django some

---
+++ ais/django-course-youtube-master/django-course-youtube-master/core/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+from .middleware import get_current_user
+from django.db.models import Q
+
+# Create your models here.
+class Articles(models.Model):
+    author = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name='Владелец достижения', blank = True, null = True )
+    create_date = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name='Статью'
+        verbose_name_plural='Статьи'
+
+class StatusFilterComments(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(Q(status=False, author = get_current_user()) | Q(status=False, article__author=get_current_user()) | Q(status=True))
+    
+
+
+class Comments(models.Model):
+    article = models.ForeignKey(Articles, on_delete = models.CASCADE, verbose_name='Статья', blank = True, null = True,related_name='comments_articles' )
+    author = models.ForeignKey(User, on_delete = models.CASCADE, verbose_name='Автор комментария', blank = True, null = True )
+    create_date = models.DateTimeField(auto_now=True)
+    text = models.TextField(verbose_name='Текст комментария')
+    status = models.BooleanField(verbose_name='Видимость статьи', default=False)
+    objects  = StatusFilterComments()
+
---
+++ ais/web-app/main.py
@@ -0,0 +1,7 @@
+import django
+
+
+if __name__ == '__main__':
+    print('PyCharm')
+
+
